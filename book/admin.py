bbb0f23946881b723ca0b1d7c57440f408f8bcb9
Kay Cha <kay@hannal.net>
2016-05-28 17:48:04 +0900

    initial commit

---
+++ book/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from book.models import Post
+
+
+admin.site.register(Post)
+
---
+++ book/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=80)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
